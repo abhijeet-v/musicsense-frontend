172e6d854dff81007a2164fea92be025681449cd
Abhijeet Verma <40263034+abhijeet-v@users.noreply.github.com>
2024-05-22 17:40:37 +0530

    Delete musicsense-frontend-master directory

--- musicsense-frontend-master/src/getres.tsx
+++
@@ -1,6 +0,0 @@
-export async function getResults(): Promise<IResType[]> {
-  return fetch("http://musicsense-backend:8000/", {
-    method: "GET",
-    redirect: "follow",
-  }).then(res=>res.json()).catch((error) => console.log(error));
-}
